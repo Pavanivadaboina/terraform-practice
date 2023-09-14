0789129e7c9cc2f1e3b2070ad643cdcffc4bac57
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-14 17:59:30 +0530

    ansible code

--- for-each/providers.tf
+++ for-each/providers.tf
@@ -1,11 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.16.2"
     }
   }
-}
+
+    backend "s3" {
+      bucket         = "terraform-bucket-3"
+      key            = "for-each-bucket"
+      region         = "us-east-1"
+      dynamodb_table = "roboshop-lock"
+    }
+  }
+
 
 
 provider "aws" {
