193c63563529da50b6d1a41c040562e39d11742d
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-14 17:57:01 +0530

    ansible code

--- for-each/providers.tf
+++ for-each/providers.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-backend "s3" {
-  bucket = "terraform-bucket-3"
-  key    = "for-each-bucket"
-  region = "us-east-1"
-  dynamodb_table = "roboshop-lock"
-}
-
 
 provider "aws" {
   # Configuration options
---
+++ for-each/state.tf
@@ -0,0 +1,6 @@
+backend "s3" {
+  bucket = "terraform-bucket-3"
+  key    = "for-each-bucket"
+  region = "us-east-1"
+  dynamodb_table = "roboshop-lock"
+}
