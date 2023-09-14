de94e90225c37175055dd045da9b079b76969624
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-14 17:03:57 +0530

    ansible code

--- for-each/var.tf
+++ for-each/var.tf
@@ -25,3 +25,32 @@ variable "zone_id" {
 variable "domain" {
   default = "devopspractice.tech"
 }
+
+variable "ingress"  {
+
+  default = [
+    {
+      from_port = 80
+      to_port = 80
+      description = "allowing PORT 80 from public"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port = 443
+      to_port = 443
+      description = "allowing PORT 443 from public"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port = 22
+      to_port = 22
+      description = "allowing PORT 22 from public"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+}
