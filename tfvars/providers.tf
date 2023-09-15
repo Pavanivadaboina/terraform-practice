e756922519f80309e8453fb9364c9997cffcab72
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-15 19:34:33 +0530

    ansible code

--- tfvars/providers.tf
+++ tfvars/providers.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "terraform-bucket-3"
     key            = "tfvars"
     region         = "us-east-1"
-    dynamodb_table = "roboshop-lock"
+    dynamodb_table = "roboshop"
 
   }
 
