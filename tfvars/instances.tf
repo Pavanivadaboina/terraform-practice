7086dc34dfc1a089f2acc8525b17dfa5c3cbf6b2
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-15 15:53:26 +0530

    ansible code

--- tfvars/instances.tf
+++ tfvars/instances.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "instances" {
+resource "aws_instance" "roboshop" {
   for_each = var.instances
   ami = var.ami_id
   instance_type = each.value
 
   tags = {
-    name = each.key
+    Name = each.key
   }
 }
 
 
 resource "aws_route53_record" "record" {
-  for_each = aws_instance.instances
+  for_each = aws_instance.roboshop
   zone_id = var.zone_id
   name    = "${each.key}.${var.domain}"
   type    = "A"
